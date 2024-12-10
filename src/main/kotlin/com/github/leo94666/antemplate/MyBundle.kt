7a160a488ecf4ce096aac512ebb32349ce6eaf28
GitHub Action <action@github.com>
2024-12-10 07:42:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/leo94666/antemplate/MyBundle.kt
+++ src/main/kotlin/com/github/leo94666/antemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.leo94666.antemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/leo94666/antemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/leo94666/antemplate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.leo94666.antemplate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.leo94666.antemplate.MyBundle
+import com.github.leo94666.antemplate.services.MyProjectService
 import javax.swing.JButton
 
 
